b03f838f7763e5b747da2c551ea00738cb3d1f85
SandraSeehars <s.seehars@gmx.ch>
2020-01-21 14:28:45 +0100

    First test for division shall force an unexpected exception

--- src/test/java/ch/bbw/ss/ConsoleCalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/ss/ConsoleCalculator/CalculatorTest.java
@@ -40,4 +40,16 @@ public class CalculatorTest {
 	public void testSubtractionTwoNegativesIsOk() {
 		assertTrue(testee.subtract(-25,  -10) == -15);
 	}
+	
+	// No exception excepted - otherwise: test has an error
+	@Test
+	public void testDivisionTwoPositivesIsOk() throws java.lang.ArithmeticException {
+		assertTrue(testee.divide(25,  5) == 5);
+	}
+	
+	// This test forces the exception and contains an error
+	@Test
+	public void testDivisionForceUnexpectedException() throws java.lang.ArithmeticException {
+		assertTrue(testee.divide(10, 0) == 5);
+	}
 }
