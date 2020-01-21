dae74be2835526638af8736196b999b6a8664e4c
SandraSeehars <s.seehars@gmx.ch>
2020-01-21 08:19:58 +0100

    Application class shall print out results in console

--- src/main/java/ch/bbw/ss/ConsoleCalculator/App.java
+++ src/main/java/ch/bbw/ss/ConsoleCalculator/App.java
@@ -1,13 +1,26 @@
 package ch.bbw.ss.ConsoleCalculator;
 
 /**
- * Hello world!
- *
+ * Application class
+ * 	- prints out results in console
+ * 
+ * @author Sandra Seehars
+ * @version 21.01.2020
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main( String[] args ) {
+    	Calculator calculator = new Calculator();
+    	int valueA = 0;
+    	int valueB = 0;
+    	
+    	
+        System.out.println("Console Calculator");
+        System.out.println("==================");
+        System.out.println();
+        
+        valueA = 10; 
+        valueB = 20;
+        System.out.println("Sum " + valueA + " + " + valueB + " = " + calculator.sum(10, 20));
     }
 }
